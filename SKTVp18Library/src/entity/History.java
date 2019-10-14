589b7ee6d1b1f4307b86c18b13fd0b145311a6e3
OLGA <oliva222@hotmail.com>
2019-10-14 13:22:59 +0300

    14/10/2019  Реализована связь с БД

---
+++ SKTVp18Library/src/entity/History.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author user
+ */
+@Entity
+public class History implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Book book;
+    @OneToOne
+    private Reader reader;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date takeOn;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date returnDate;
+
+    public History() {
+    
+    
+}
+
+    public History(Long id, Book book, Reader reader, Date takeOn, Date returnDate) {
+        this.id = id;
+        this.book = book;
+        this.reader = reader;
+        this.takeOn = takeOn;
+        this.returnDate = returnDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public Date getTakeOn() {
+        return takeOn;
+    }
+
+    public void setTakeOn(Date takeOn) {
+        this.takeOn = takeOn;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    @Override
+    public String toString() {
+        return "History{" + "id=" + id + ", book=" + book + ", reader=" + reader + ", takeOn=" + takeOn + ", returnDate=" + returnDate + '}';
+    }
+}
